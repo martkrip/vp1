6e665dfd76f0317741917589aa64eaf5cba24e03
Martin Toomas Kripsaar <martkrip@users.noreply.github.com>
2024-10-03 15:57:57 +0300

    regvisit, timenow, justlist, views ja muud

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dtEt = require("./dateTime.js");
-
+const fs = require("fs")
+const bodyparser = require("body-parser");
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+//päringu URL-i parsimine , false kui ainult tekst, true, kui muud ka
+app.use(bodyparser.urlencoded({extended: false})); //dekodeeri asju
 
 app.get("/", (req, res)=>{
     //res.send("Express läks täiesti käima!");
@@ -11,10 +14,47 @@ app.get("/", (req, res)=>{
 });
 
 app.get("/timenow", (req, res)=>{
-    const weekdayNow =dtEt.weekDay();
-    const dateNow = dtEt.dateFormatted();
-    const timeNow =dtEt.timeFormatted();
+    const weekdayNow =dtEt.weekDayEt();
+    const dateNow = dtEt.dateEt();
+    const timeNow =dtEt.timeEt();
     res.render("timenow", {nowWD: weekdayNow, nowD: dateNow, nowT: timeNow}); //saatan muutujad mailile?
 
 });
+
+app.get("/vanasonad", (req, res)=>{
+    let folkWisdom =[];
+    fs.readFile("public/textfiles/vanasonad.txt", "utf8", (err, data)=>{
+        if(err){
+            //throw err;
+            res.render ("justlist", {h2: "Vanasõnad", listData: ["Ei leidnud ühtegi vanasõna!"]});
+        }
+        else {
+            folkWisdom = data.split(";");
+            res.render("justlist", {h2: "Vanasõnad", listData: folkWisdom});
+        }
+    });
+})
+app.get("/regvisit", (req, res)=>{
+    res.render("regvisit");
+})
+
+app.post("/regvisit", (req, res)=>{
+    console.log(req.body);
+    fs.open("public/textfiles/visitlog.txt", "a", (err, file)=>{
+        if(err){
+            throw err
+        }
+        else {
+            fs.appendFile("public/textfiles/visitlog.txt", req.body.firstNameInput + " " + req.body.lastNameInput + ";", (err)=>{
+                if(err){
+                    throw err
+                }
+                else {
+                    console.log("Faili kirjutati!");
+                    res.render("regvisit")
+                }
+            })
+        }
+    });
+})
 app.listen(5127);
