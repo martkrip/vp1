481bb0139b0a12969896a6049868d6adf5d2f157
Martin Toomas Kripsaar <martkrip@users.noreply.github.com>
2024-10-03 13:54:10 +0300

    vp1 asju

---
+++ dateTime.js
@@ -0,0 +1,50 @@
+//exports.weekDayEt = function(){ //miks puudub eksport rinde failis ja on olemas date.js?
+const monthNamesEt = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+const weekdayNamesEt = ["pühapäev", "esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev"];
+
+const DateEt = function(){
+    let timeNow = new Date();
+    console.log ("Praegu on: " + timeNow);
+    let dateNow = timeNow.getDate();
+    let monthNow = timeNow.getMonth();
+    let yearNow = timeNow.getFullYear();
+    let dateNowEt = dateNow + ". " + monthNamesEt[monthNow] + " " + yearNow;
+    return dateNowEt;
+}
+const weekDayEt = function(){
+    let timeNow = new Date();
+    let dayNow = timeNow.getDay();
+    return weekdayNamesEt[dayNow];
+}
+const timeFormattedET = function(){
+    let timeNow = new Date();
+    let hourNow = timeNow.getHours();
+    let minuteNow = timeNow.getMinutes();
+    let secondNow = timeNow.getSeconds();
+    return hourNow + ":" + minuteNow + ":" + secondNow;
+}
+const partOfDayEt = function(){
+    let dayPart = "suvaline hetk";
+    let timeNow = new Date();
+    if(timeNow.getHours() >= 8 && timeNow.getHours() < 16) {
+        dayPart = "kooliaeg";
+    }
+    else if(timeNow.getHours()>= 21&& timeNow.getHours() < 24) {
+        dayPart = "uneaeg"
+    }
+    else if(timeNow.getHours()>= 0&& timeNow.getHours() < 7) {
+        dayPart = "uneaeg öösel"
+    }
+    else if(today.getDay() == 6  || today.getDay() == 0 && timeNow.getHours() >= 8 && timeNow.getHours() <=21) {
+        dayPart = "puhkeaeg"
+    }
+    else if(today.getDay() == 6  || today.getDay() == 0 && timeNow.getHours() >= 21 && timeNow.getHours() <=24) {
+        dayPart = "uneaeg nädalavahetustel"
+    }
+    else if(today.getDay() == 6  || today.getDay() == 0 && timeNow.getHours() >= 0 && timeNow.getHours() <=7) {
+        dayPart = "uneaeg öösel nädalavahetustel"
+    }
+    return dayPart;
+
+}
+module.exports = {monthsEt: monthNamesEt, weekdaysEt: weekdayNamesEt, dateEt: DateEt, weekDayEt: weekDayEt, timeEt: timeFormattedET, partOfDayEt};
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const dtEt = require("./dateTime.js");
+
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res)=>{
-    res.send("Express läks täiesti käima!");
+    //res.send("Express läks täiesti käima!");
+    res.render("index.ejs");
 });
 
+app.get("/timenow", (req, res)=>{
+    const weekdayNow =dtEt.weekDay();
+    const dateNow = dtEt.dateFormatted();
+    const timeNow =dtEt.timeFormatted();
+    res.render("timenow", {nowWD: weekdayNow, nowD: dateNow, nowT: timeNow}); //saatan muutujad mailile?
+
+});
 app.listen(5127);
